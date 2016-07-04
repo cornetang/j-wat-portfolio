80cd25351f84f5d20d941285edf72ca89b4d0158
Corne <sctang.stu@gmail.com>
2016-07-04 22:44:09 +0800

    generate array

--- js/app.js
+++ js/app.js
@@ -8,12 +8,18 @@
     		imgsrc: "img/portfolio-01.jpg",
         count: 10,
         name: "ad",
+        imgArray: [],
+        imgnumArray: [
+          "a", "b", "c",
+        ],
     	},
     	{ 
     		linksrc: "portfolio-illustration.html",
     		imgsrc: "img/portfolio-02.jpg",
         count: 25,
         name: "illu",
+        imgArray: [],
+        imgnumArray: [],
     	},
     	{ 
     		linksrc: "portfolio-showreel.html",
@@ -24,9 +30,22 @@
     		imgsrc: "img/portfolio-04.jpg",
         count: 3,
         name: "free",
+        imgArray: [],
+        imgnumArray: [],
     	}
     ];
+
+    this.pages.forEach(function(entry){
+      for (i=1;i<entry.count;i++) {
+        entry.imgArray[i-1] = "img/" + entry.name + i + ".jpg";
+        entry.imgnumArray[i-1] = "img/" + entry.name + "num" + i + ".jpg";
+      }
+    });
+
+
   });
+
+
 
   app.directive('about', function(){
     return {
