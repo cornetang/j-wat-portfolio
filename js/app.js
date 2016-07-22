552ad7297a7a02753c9248fb48f755608d1df794
Corne <sctang.stu@gmail.com>
2016-07-23 00:30:32 +0800

    refractored to angularjs

--- js/app.js
+++ js/app.js
@@ -1,25 +1,74 @@
 (function(){
   
   var app = angular.module('portfolio', []);
+
+  app.controller('TabController', function(){
+    this.tab = 1;
+    this.slide = -1;
+    this.portfo = -1;
+
+    this.setTab = function(tabno) {
+        this.tab = tabno; 
+        this.slide = -1; 
+        this.portfo = -1;s
+    }
+
+    this.checkTab = function(tabno) {
+        return this.tab === tabno;
+    }
+
+    this.checkSlide = function(slideno) {
+        return this.slide === slideno;
+    }
+
+    this.setSlide = function(index) {
+        this.slide = 1;
+        this.portfo = index;
+    }
+
+    this.getPortfo = function(){
+        return this.portfo;
+    }
+
+  });
+
   app.controller('PortfolioController', function(){
+    this.imgno = 0;
+
+    this.resetImgNo = function() {
+        this.imgno = 0;
+    }
+
+    this.increment = function(portno) {
+        this.imgno = this.imgno + 1;
+        if (this.imgno === this.pages[portno].count) this.imgno = 0;
+    }
+
+    this.decrement = function(portno) {
+        this.imgno = this.imgno - 1;
+        if (this.imgno === -1) this.imgno = this.pages[portno].count - 1;
+    }
+
+    this.getImgNo = function() {
+        return this.imgno;
+    }
+
     this.pages = [
       	{ 
     		linksrc: "portfolio-animation.html",
     		imgsrc: "img/portfolio-01.jpg",
-        count: 10,
-        name: "ad",
-        imgArray: [],
-        imgnumArray: [
-          "a", "b", "c",
-        ],
+            count: 10,
+            name: "ad",
+            imgArray: [],
+            imgnumArray: [],
     	},
     	{ 
     		linksrc: "portfolio-illustration.html",
     		imgsrc: "img/portfolio-02.jpg",
-        count: 25,
-        name: "illu",
-        imgArray: [],
-        imgnumArray: [],
+            count: 25,
+            name: "illu",
+            imgArray: [],
+            imgnumArray: [],
     	},
     	{ 
     		linksrc: "portfolio-showreel.html",
@@ -28,19 +77,23 @@
     	{ 
     		linksrc: "portfolio-freelance.html",
     		imgsrc: "img/portfolio-04.jpg",
-        count: 3,
-        name: "free",
-        imgArray: [],
-        imgnumArray: [],
+            count: 3,
+            name: "free",
+            imgArray: [],
+            imgnumArray: [],
     	}
     ];
 
     this.pages.forEach(function(entry){
-      for (i=1;i<entry.count;i++) {
+      for (i=1;i<=entry.count;i++) {
         entry.imgArray[i-1] = "img/" + entry.name + i + ".jpg";
         entry.imgnumArray[i-1] = "img/" + entry.name + "num" + i + ".jpg";
       }
     });
+
+    this.copyright = function(){
+      $('.copyright').append("abc");
+    }
 
 
   });
