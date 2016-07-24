987f3221b6e54ec0a1fb8cecbeb3b8980945a027
Corne <sctang.stu@gmail.com>
2016-07-25 00:01:46 +0800

    add youtube frame

--- js/app.js
+++ js/app.js
@@ -106,7 +106,7 @@
         this.preload = function(portno){
             for (i=0; i<this.pages[portno].count; i++){
                 (new Image()).src = this.pages[portno].imgArray[i];
-                $('.testing').append(this.pages[portno].imgArray[i]);
+                // $('.testing').append(this.pages[portno].imgArray[i]);
             }
         }
 
